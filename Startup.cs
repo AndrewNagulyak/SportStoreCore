ce0d3b9dd21265c4d61be824b7c02d3cef0dcb3f
Andrew Naholik <andriinahyliak58@gmail.com>
2019-03-19 13:04:46 +0200

    hotfix test

--- Startup.cs
+++ Startup.cs
@@ -18,8 +18,7 @@ namespace SportStoreCore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+   
         public Startup(IConfiguration configuration) =>
             Configuration = configuration;
 
